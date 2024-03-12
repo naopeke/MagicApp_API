23329f7b239ea194f1120e144536bdf919d0daa7
naopeke <naopeke@protonmail.com>
2024-03-12 17:16:34 +0100

    changed password

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 const connection = mysql.createConnection({
     host:       process.env.DB_HOST         || "magydeck.c92woie8epi1.eu-west-3.rds.amazonaws.com",
     user:       process.env.DB_USER         || "admin",
-    password:   process.env.DB_PASSWORD     || "magydeck2024_",
+    password:   process.env.DB_PASSWORD     || "magydeck2024*",
     database:   process.env.DB_NAME         || "magydeck",
     port:       process.env.DB_PORT         || 3306
 })
