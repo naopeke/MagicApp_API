4d9d8155a06c949cea9363445d7bf3dc1d4fc861
naopeke <naopeke@protonmail.com>
2024-03-13 07:25:34 +0100

    in progress

---
+++ src/routers/user.routers.js
@@ -0,0 +1,11 @@
+const { Router } = require ('express');
+const router = Router();
+const userCtrl = require('../controller/user.controller');
+
+
+// router.post('/register', userCtrl.registerUser);
+
+// router.post('/login', userCtrl.loginUser);
+
+// router.put('/profile', userCtrl.editUser);
+
