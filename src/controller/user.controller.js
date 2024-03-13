a4b8f898dbcbf0351c2b41b22459d4b62e790220
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-03-13 21:31:22 +0100

    login

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,20 +1,71 @@
 const { connection } =  require('../database');
 
 const registerUser = async (req, res, next) => {
+    const respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "La operación se ha realizado con éxito"
+    };
+
+    const user = {
+        nameUser: 'Nombre',
+        emailUser: 'email',
+        passwordUser: 'contraseña'
+    }
+
     try {
+        let sqlEmail = "SELECT emailUser from user WHERE emailUser = '" + user.emailUser + "'";
+        let [checkEmail] = await pool.query(sqlEmail);
+        if(checkEmail.length){
+            respuesta.error = true;
+            respuesta.codigo = 400; 
+            respuesta.mensaje = 'Ya existe un usuario con este email';
+        } else {
+            let sql = "INSERT INTO user (nameUser, emailUser, passwordUser)" +
+            "VALUES ('" + user.nameUser + "', '" +
+            user.emailUser + "','" +
+            user.passwordUser + "')";
+            await pool.query(sql);
+            let sqlUser = "SELECT id_user FROM user WHERE emailUser = '" + user.emailUser + "'";
+            let [userId] = await pool.query(sqlUser);
+            user.id_user = userId[0].id_user;
+            user.passwordUser = null;
+        }
+        res.send(respuesta);
         console.log('register try');
-    } catch {
+    }catch(err){
+        console.error(err);
         console.log('register catch');
     }
 }
 
 const loginUser = async (req, res, next) => {
+    const respuesta = {
+        error: false,
+        codigo: 200,
+        mensaje: "La operación se ha realizado con éxito",
+        data: null
+    };
+
     try {
+        let sql = "SELECT * FROM user WHERE user.emailUser = '" + req.body.emailUser + "' AND user.passwordUser = '" + req.body.passwordUser + "'";
+        let [result] = await pool.query(sql);
+        if(result.length){
+            respuesta.data = result.length ? result[0] : null;
+        } else {
+            respuesta.error = true;
+            respuesta.mensaje = "Login incorrecto";
+            respuesta.codigo = 400; 
+        }
+        res.send(respuesta);
         console.log('login try');
     } catch {
+
         console.log('login catch');
     }
 }
+
+
 
 const logoutUser = (req, res, next) => {
     res.json({ success: true });
@@ -24,5 +75,7 @@ const logoutUser = (req, res, next) => {
 module.exports = {
     registerUser,
     loginUser,
+    // getUser,
+    // editUser,
     logoutUser
 };
