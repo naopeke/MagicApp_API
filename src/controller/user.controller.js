4379d475b0973e71a4e5133e13edd3c4b7275b09
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-03-14 11:49:02 +0100

    eindpoint register arreglado

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,4 +1,5 @@
-const { connection } =  require('../database');
+const { pool } =  require('../database');
+
 
 
 const registerUser = async (req, res, next) => {
@@ -17,23 +18,23 @@ const registerUser = async (req, res, next) => {
     try {
         user = req.body; 
 
-        let sqlEmail = "SELECT emailUser from user WHERE emailUser = '" + user.emailUser + "'";
+        let sqlEmail = "SELECT emailUser from magydeck.user WHERE emailUser = '" + user.emailUser + "'";
         let [checkEmail] = await pool.query(sqlEmail);
         if(checkEmail.length){
             respuesta.error = true;
             respuesta.codigo = 400; 
             respuesta.mensaje = 'Ya existe un usuario con este email';
         } else {
-            let sql = "INSERT INTO user (nameUser, emailUser, passwordUser)" +
+            let sql = "INSERT INTO magydeck.user (nameUser, emailUser, passwordUser)" +
             "VALUES ('" + user.nameUser + "', '" +
             user.emailUser + "','" +
             user.passwordUser + "')";
             console.log(sql);
             await pool.query(sql);
-            let sqlUser = "SELECT id_user FROM user WHERE emailUser = '" + user.emailUser + "'";
-            let [userId] = await pool.query(sqlUser);
-            user.id_user = userId[0].id_user;
-            user.passwordUser = null;
+            // let sqlUser = "SELECT id_user FROM user WHERE emailUser = '" + user.emailUser + "'";
+            // let [userId] = await pool.query(sqlUser);
+            // user.id_user = userId[0].id_user;
+            // user.passwordUser = null;
         }
         res.send(respuesta);
         console.log('register try');
@@ -46,31 +47,31 @@ const registerUser = async (req, res, next) => {
 
 
 
-const loginUser = async (req, res, next) => {
-    const respuesta = {
-        error: false,
-        codigo: 200,
-        mensaje: "La operación se ha realizado con éxito",
-        data: null
-    };
+// const loginUser = async (req, res, next) => {
+//     const respuesta = {
+//         error: false,
+//         codigo: 200,
+//         mensaje: "La operación se ha realizado con éxito",
+//         data: null
+//     };
 
-    try {
-        let sql = "SELECT * FROM user WHERE user.emailUser = '" + req.body.emailUser + "' AND user.passwordUser = '" + req.body.passwordUser + "'";
-        let [result] = await pool.query(sql);
-        if(result.length){
-            respuesta.data = result.length ? result[0] : null;
-        } else {
-            respuesta.error = true;
-            respuesta.mensaje = "Login incorrecto";
-            respuesta.codigo = 400; 
-        }
-        res.send(respuesta);
-        console.log('login try');
-    } catch {
+//     try {
+//         let sql = "SELECT * FROM user WHERE user.emailUser = '" + req.body.emailUser + "' AND user.passwordUser = '" + req.body.passwordUser + "'";
+//         let [result] = await pool.query(sql);
+//         if(result.length){
+//             respuesta.data = result.length ? result[0] : null;
+//         } else {
+//             respuesta.error = true;
+//             respuesta.mensaje = "Login incorrecto";
+//             respuesta.codigo = 400; 
+//         }
+//         res.send(respuesta);
+//         console.log('login try');
+//     } catch {
 
-        console.log('login catch');
-    }
-}
+//         console.log('login catch');
+//     }
+// }
 
 
 
@@ -81,7 +82,7 @@ const logoutUser = (req, res, next) => {
 
 module.exports = {
     registerUser,
-    loginUser,
+    // loginUser,
     // getUser,
     // editUser,
     logoutUser
--- src/routers/user.routers.js
+++ src/routers/user.routers.js
@@ -6,7 +6,7 @@ const userCtrl = require('../controller/user.controller.js');
 
 router.post('/register', userCtrl.registerUser);
 
-router.post('/login', userCtrl.loginUser);
+// router.post('/login', userCtrl.loginUser);
 
 
 
