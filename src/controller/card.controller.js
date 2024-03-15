19fe1b1b91ce08a0c279660b32f7fb7d524f84df
naopeke <naopeke@protonmail.com>
2024-03-15 10:00:35 +0100

    added printed_name

--- src/controller/card.controller.js
+++ src/controller/card.controller.js
@@ -12,6 +12,7 @@ const fetchCardData = async (req, res, next) => {
                 id: response.data.id,
                 image_uris: response.data.image_uris.normal, 
                 name: response.data.name,
+                printed_name: response.data.printed_name,
                 type_line: response.data.type_line,
                 oracle_text: response.data.oracle_text,
                 printed_text: response.data.printed_text,
@@ -35,6 +36,37 @@ const fetchCardData = async (req, res, next) => {
 //500 Internal Server Error
 //400 Bad Request
 
+
+
+// const fetchCollectiondData = async (req, res, next) => {
+//     if (req.query.cardName) {
+//         // GET https://api.scryfall.com/cards/named?fuzzy=aust+com  hay que cambiar espacio por '+'
+
+//         const cardName = req.query.cardName.split(' ').join('+');
+//         try {
+//             const response = await axios.get(`https://api.scryfall.com/cards/named?fuzzy=${encodeURI(cardName)}`);
+//             const cardData = {
+//                 id: response.data.id,
+//                 image_uris: response.data.image_uris.normal, 
+//                 name: response.data.name,
+//                 type_line: response.data.type_line,
+//                 oracle_text: response.data.oracle_text,
+//                 printed_text: response.data.printed_text,
+//                 color_identity: response.data.color_identity,
+//                 legalities: response.data.legalities,
+//                 set_name: response.data.set_name,
+//                 set_type: response.data.set_type,
+//                 prices: response.data.prices.eur
+//             };
+//             res.json(cardData); // mandar en formato json
+//         } catch (err) {
+//             console.log('Error fetching', err);
+//             res.status(500).send('Error in fetching card data');
+//         }
+//     } else {
+//         res.status(400).send('Card name is required');
+//     }
+// };
 
 
 const addCards = async (req, res, next) => {
