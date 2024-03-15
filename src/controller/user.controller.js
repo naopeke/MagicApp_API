5ff24c7e04846c323ba65feb75c361d430499962
xisca-ayala <xisca.ayala@gmail.com>
2024-03-15 11:46:00 +0100

    login exito

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -11,7 +11,7 @@ const registerUser = async (req, res, next) => {
     let user = {
         nameUser: 'Nombre',
         emailUser: 'email',
-        passwordUser: 'contraseña'
+        passwordUser: 'contraseña',
     }
 
     try {
@@ -46,31 +46,33 @@ const registerUser = async (req, res, next) => {
 
 
 
-// const loginUser = async (req, res, next) => {
-//     const respuesta = {
-//         error: false,
-//         codigo: 200,
-//         mensaje: "La operación se ha realizado con éxito",
-//         data: null
-//     };
+const loginUser = async (req, res, next) => {
+    const response = {
+        err: false,
+        code: 200,
+        message: "La operación se ha realizado con éxito",
+        data: null
+    };
 
-//     try {
-//         let sql = "SELECT * FROM user WHERE user.emailUser = '" + req.body.emailUser + "' AND user.passwordUser = '" + req.body.passwordUser + "'";
-//         let [result] = await pool.query(sql);
-//         if(result.length){
-//             respuesta.data = result.length ? result[0] : null;
-//         } else {
-//             respuesta.error = true;
-//             respuesta.mensaje = "Login incorrecto";
-//             respuesta.codigo = 400; 
-//         }
-//         res.send(respuesta);
-//         console.log('login try');
-//     } catch {
-
-//         console.log('login catch');
-//     }
-// }
+    try {
+        let sql = "SELECT * FROM magydeck.user WHERE user.emailUser = '" + req.body.emailUser + "' AND user.passwordUser = '" + req.body.passwordUser + "'";
+        let [result] = await pool.query(sql);
+        if(result.length){
+            response.data = result.length ? result[0] : null;
+        } else {
+            response.err = true;
+            response.message = "Login incorrecto";
+            response.code = 400; 
+        }
+        console.log('login try');
+    } catch (error){
+        response.err = true; 
+        response.message = "Ocurrió un error";
+        response.code = 500; 
+        console.error('login catch', error);
+    }
+    res.send(response); 
+}
 
 
 
@@ -81,7 +83,7 @@ const logoutUser = (req, res, next) => {
 
 module.exports = {
     registerUser,
-    // loginUser,
+    loginUser,
     // getUser,
     // editUser,
     logoutUser
