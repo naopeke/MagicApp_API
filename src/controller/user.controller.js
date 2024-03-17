55397641e503bb311b936c211f75242570297ec4
naopeke <naopeke@protonmail.com>
2024-03-17 10:38:00 +0100

    resolved conflict user.routers3

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -29,6 +29,34 @@ const registerUser = async (req, res, next) => {
             user.passwordUser + "')";
             console.log(sql);
             await pool.query(sql);
+
+            //https://www.w3schools.com/sql/func_mysql_last_insert_id.asp
+            // obtener el ultimo id_user, y crear 5 mazos con el ultimo id_user
+            let latestIdUser = "SELECT LAST_INSERT_ID() as id_user;"
+            console.log(latestIdUser);
+            let [latestIdUserResult] = await pool.query(latestIdUser);
+            console.log(latestIdUserResult);
+            const userIdParam = [latestIdUserResult[0].id_user];
+
+            let addDeck1 = "INSERT INTO magydeck.deck (id_user, nameDeck, id_photoDeck, share, mediaScore) VALUES (?, 'mimazo1', 1, 0, 0)";
+            console.log(addDeck1);
+            await pool.query(addDeck1, userIdParam);
+            
+            let addDeck2 = "INSERT INTO magydeck.deck (id_user, nameDeck, id_photoDeck, share, mediaScore) VALUES (?, 'mimazo2', 2, 0, 0)";
+            console.log(addDeck2);
+            await pool.query(addDeck2, userIdParam);
+            
+            let addDeck3 = "INSERT INTO magydeck.deck (id_user, nameDeck, id_photoDeck, share, mediaScore) VALUES (?, 'mimazo3', 3, 0, 0)";
+            console.log(addDeck3);
+            await pool.query(addDeck3, userIdParam);
+            
+            let addDeck4 = "INSERT INTO magydeck.deck (id_user, nameDeck, id_photoDeck, share, mediaScore) VALUES (?, 'mimazo4', 4, 0, 0)";
+            console.log(addDeck4);
+            await pool.query(addDeck4, userIdParam);
+            
+            let addDeck5 = "INSERT INTO magydeck.deck (id_user, nameDeck, id_photoDeck, share, mediaScore) VALUES (?, 'mimazo5', 5, 0, 0)";
+            console.log(addDeck5);
+            await pool.query(addDeck5, userIdParam);
         }
         res.status(200).send(respuesta);
         console.log('register try');
@@ -43,13 +71,6 @@ const registerUser = async (req, res, next) => {
     }
 }
 
-const loginUser = async (req, res, next) => {
-    const respuesta = {
-        error: false,
-        codigo: 200,
-        mensaje: "La operación se ha realizado con éxito",
-        data: null
-    };
 const loginUser = async (req, res, next) => {
     const response = {
         err: false,
@@ -81,7 +102,5 @@ const loginUser = async (req, res, next) => {
 
 module.exports = {
     registerUser,
-    loginUser,
-    loginUser,
-
+    loginUser
 };
